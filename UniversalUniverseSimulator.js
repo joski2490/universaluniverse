7410252a4daaf08062ee6eb39adacd9fdcd69d5c
Steven Velozo <steven@velozo.com>
2014-03-07 10:21:16 -0800

    Made the command-line parameter not wishful thinking.

--- UniversalUniverseSimulator.js
+++ UniversalUniverseSimulator.js
@@ -8,11 +8,21 @@
 *
 * @description Simulator server.  Bork bork.
 */
-
+var Minimist = require('minimist');
 var Connect = require('connect');
 var ServeStatic = require('serve-static');
+
+var Settings = { };
+
+var CommandLineArguments = Minimist(process.argv);
+
+// Meh we aren't gonna check for errors in this
+if (typeof(CommandLineArguments.p) === 'undefined')
+	Settings.Port = 9999;
+else
+	Settings.Port = CommandLineArguments.p;
 
 var Application = Connect();
 
 Application.use(ServeStatic('build/site', {'index': 'index.html'}));
-Application.listen(9999);
+Application.listen(Settings.Port);
